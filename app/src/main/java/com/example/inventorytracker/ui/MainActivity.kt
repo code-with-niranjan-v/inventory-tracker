cdc351cfc04ac72fdc6a3f2dc80abb445edb6fe6
Niranjan V <rathinamniranjan@gmail.com>
2024-08-04 15:47:57 +0530

    added navigation and created fragments

--- app/src/main/java/com/example/inventorytracker/ui/MainActivity.kt
+++ app/src/main/java/com/example/inventorytracker/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.inventorytracker
+package com.example.inventorytracker.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.inventorytracker.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
