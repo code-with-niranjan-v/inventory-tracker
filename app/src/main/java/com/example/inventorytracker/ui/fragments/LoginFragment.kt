596e1302abf2df5004b0de59596c2ffe59c57bd4
Niranjan V <rathinamniranjan@gmail.com>
2024-08-04 20:19:20 +0530

    added login and signup ui

--- app/src/main/java/com/example/inventorytracker/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/inventorytracker/ui/fragments/LoginFragment.kt
@@ -5,18 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.inventorytracker.R
+import com.example.inventorytracker.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
-
+    private lateinit var loginBinding: FragmentLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        loginBinding = FragmentLoginBinding.inflate(inflater,container,false)
+        return loginBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loginBinding.tvdont.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+        }
     }
 
 }
--- app/src/main/java/com/example/inventorytracker/ui/fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/inventorytracker/ui/fragments/SignUpFragment.kt
@@ -5,18 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.inventorytracker.R
+import com.example.inventorytracker.databinding.FragmentSignUpBinding
 
 
 class SignUpFragment : Fragment() {
 
+    private lateinit var signUpBinding: FragmentSignUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        signUpBinding = FragmentSignUpBinding.inflate(inflater,container,false)
+        return signUpBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        signUpBinding.tvAlready.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
+        }
     }
 
 
